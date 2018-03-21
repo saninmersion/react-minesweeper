91bc453eaf6cff9da2804f46b8a23e233d8055af
Sandip Shrestha <sandip.shrestha@yipl.com.np>
2018-03-21 09:49:25 +0545

    [MS-01] - Empty Board layout Creation 	[x] add square matrix board layout for 8x8 grid. 	[x] add spaces, mines, flags. 	[x] add a overlay on cells to hide all the cells initially

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './components/Board';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Game extends React.Component {
+
+    /*
+    Beginner: 10 mines, 8x8 board
+    Intermediate: 40 mines, 16x16 board
+    Expert: 99 mines, 16x30 board
+    */
+    state = {
+        height: 16,
+        mines: 40,
+        width: 16,
+    };
+
+    render() {
+        const {height, width, mines} = this.state;
+        return (
+            <div className="game">
+                <Board height={height} width={width} mines={mines}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
